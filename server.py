bd3c071eb8fb0fa7daa87f7c3c49b763705a03f7
wastewaste1128-spec <waste.waste1128@gmail.com>
2025-11-24 12:17:24 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,15 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Dhan Auto Exit Bot Running!"
+    return "Dhan Auto Exit Bot Running"
 
 @app.route("/start")
 def start_bot():
-    # run monitoring in background so HTTP response returns quickly
     thread = threading.Thread(target=start_monitoring)
     thread.start()
     return "Auto Exit Started for latest intraday options position."
 
-if __name__ == "__main__":
-    # for local debug; on Render you’ll use gunicorn
-    app.run(host="0.0.0.0", port=10000)
